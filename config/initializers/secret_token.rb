91a5306b28bc0ed87e8a44a051c6a06b0d7e85d4
David Taylor <david@cloudartisan.com>
2011-04-25 22:34:40 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shortstop::Application.config.secret_token = 'b0fdad440208e9c6d647e6d8c7f86e49438b00a78399370ab6f626c3e500b38f50b934cbf689ce7fbcc2a78fda03133628235441d57bf3aaf3894aef05b7ccb0'
