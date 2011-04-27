cb3f9be7bbf19e2d20dbe01a3573043c15fe06cf
David Taylor <david@cloudartisan.com>
2011-04-27 10:36:23 +1000

    Added URL validation

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,6 @@
 class Url < ActiveRecord::Base
   validates :url, :presence => true
+  validates_url_format_of :url,
+                          :allow_nil => true,
+                          :message => 'is completely unacceptable'
 end
