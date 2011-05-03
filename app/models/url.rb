c3dce9b39affdb75ca29f5612b4956a7dca3c3f2
David Taylor <david@cloudartisan.com>
2011-05-03 13:19:40 +1000

    Added methods for shortening/reversing

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,32 @@
+# Use the model's ID (PK/Serial) to generate a token which can be reversed:
+#
+#   Url.find(7213).shortened        #=> 5kd
+#   Url.find_by_shortened("5kd")    #=> #<Url id: 7213...>
+#
+# Based on this gist:
+#
+#   https://gist.github.com/581399#file_active_record_url_shortening.rb
 class Url < ActiveRecord::Base
+  Radix = 36
+
   validates :url, :presence => true
   validates_url_format_of :url,
                           :allow_nil => true,
                           :message => 'is completely unacceptable'
+
+  # Convert URL ID to a shortened version
+  def shortened
+    id.to_s(Radix)
+  end
+
+  # Unconvert the shortened version back to the URL's ID
+  def self.shortened_to_id(shortened)
+    shortened.to_i(Radix)
+  end
+
+  # Unconvert shortened back to the URL's ID and lookup 
+  def self.find_by_shortened(shortened)
+    find shortened_to_id(shortened)
+  end
+
 end
